dc2609fd6b6cb9e10e4f937adb178df9d181918d
k0gang <152839409+k0gang@users.noreply.github.com>
2024-05-15 12:53:28 +0900

    update header

--- src/routes/Header.js
+++ src/routes/Header.js
@@ -1,66 +1,94 @@
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, Button, Heading } from '@chakra-ui/react';
-import React from 'react';
 import {
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    MenuItemOption,
-    MenuGroup,
-    MenuOptionGroup,
-    MenuDivider,
-} from '@chakra-ui/react';
-import VideoList from '../components/VideoList';
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Button,
+  Heading,
+  HStack,
+  Icon,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+import { AiFillSun, AiFillMoon } from "react-icons/ai";
 
 const Header = () => {
-    return (
-        <>
-            <Heading>검색 서비스</Heading>
+  const { colorMode, toggleColorMode } = useColorMode();
+  const color = useColorModeValue("red.500", "white");
+
+  return (
+    <>
+      <header>
+        <Heading textAlign={"center"} size={"xl"} m={"20px"} color={color}>
+          <Link to={"/"}>
+            <Icon as={FaSearch} />
+            검색 서비스
+          </Link>
+        </Heading>
+        <HStack justifyContent={"space-between"}>
+          <HStack>
+            <Button>
+              <Link to={"/"}>Home</Link>
+            </Button>
             <Menu>
-                <MenuButton
-                    px={4}
-                    py={2}
-                    transition="all 0.2s"
-                    borderRadius="md"
-                    borderWidth="1px"
-                    _hover={{ bg: 'gray.400' }}
-                    _expanded={{ bg: 'blue.400' }}
-                    _focus={{ boxShadow: 'outline' }}
-                >
+              {({ isOpen }) => (
+                <>
+                  <MenuButton isActive={isOpen} as={Button}>
                     Video
-                </MenuButton>
-                <MenuList>
-                    <MenuItem as="a" href="/demo/video/list">
-                        추천 영상
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem>
+                      <Link to={"/video/list"}>추천 영상</Link>
                     </MenuItem>
-                    <MenuItem as="a" href="/demo/video">
-                        영상 검색
+                    <MenuItem>
+                      <Link to={"/video"}>영상 검색</Link>
                     </MenuItem>
-                </MenuList>
+                  </MenuList>
+                </>
+              )}
             </Menu>
             <Menu>
-                <MenuButton
-                    px={4}
-                    py={2}
-                    transition="all 0.2s"
-                    borderRadius="md"
-                    borderWidth="1px"
-                    _hover={{ bg: 'gray.400' }}
-                    _expanded={{ bg: 'blue.400' }}
-                    _focus={{ boxShadow: 'outline' }}
-                >
+              {({ isOpen }) => (
+                <>
+                  <MenuButton isActive={isOpen} as={Button}>
                     Book
-                </MenuButton>
-                <MenuList>
-                    <MenuItem as="a" href="/demo/book/list">
-                        추천 도서
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem>
+                      <Link to={"/book/list"}>추천 도서</Link>
                     </MenuItem>
-                    <MenuItem as="a" href="/demo/book">
-                        도서 검색
+                    <MenuItem>
+                      <Link to={"/book"}>도서 검색</Link>
                     </MenuItem>
-                </MenuList>
+                  </MenuList>
+                </>
+              )}
             </Menu>
-        </>
-    );
+          </HStack>
+          {colorMode === "light" ? (
+            <IconButton
+              icon={<AiFillMoon />}
+              onClick={toggleColorMode}
+              size={"lg"}
+            />
+          ) : (
+            <IconButton
+              icon={<AiFillSun />}
+              onClick={toggleColorMode}
+              size={"lg"}
+            />
+          )}
+        </HStack>
+      </header>
+    </>
+  );
 };
+
 export default Header;
