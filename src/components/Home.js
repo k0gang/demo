fa402f3df203b624ddd478045ac446edf930acf0
k0gang <152839409+k0gang@users.noreply.github.com>
2024-05-15 12:52:32 +0900

    Add : Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,7 @@
-import React, { useState } from 'react';
+import React from "react";
 
-const Home = ({ style }) => {
-    // State : Component에 제공하는 상태 값
-    // ㄴ 화면 랜더링에 영향 O
-    const [count, setCount] = useState(1);
-    console.log(count);
-
-    const countUp = (e) => {
-        setCount(count + 1);
-    };
-
-    return (
-        <>
-            <button style={style} onClick={countUp}>
-                {count}
-            </button>
-        </>
-    );
+const Home = () => {
+  return <div>MAIN</div>;
 };
 
 export default Home;
